e498bbe039cb5e6810d592c4d02175f1713b1d7f
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-14 16:57:27 -0400

    Add files via upload

---
+++ example3.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+vector<string> justapythonlist ={ "hello", "world", "bye", "planet"
+};int a ;
+cin >> a ;string b ;
+cin >> b ;
+cout << "The integer input is: "  <<  a << endl;
+cout << "The string input is: "  <<  b << endl;
+
+}
---
+++ example3.py
@@ -0,0 +1,6 @@
+justapythonlist = ["hello", "world", "bye", "planet"]
+a = int(input())
+b = input()
+
+print("The integer input is: " + a)
+print("The string input is: " + b)
