76edfdd09c99788b1a6eebfc6164dfcbe1b56959
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-14 17:15:09 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pytocpp',
+    version='0.1',
+    author="Savir Singh",
+    description='Convert Python Code To C++ Code',
+    author_email='kopichiki@gmail.com',
+    url='https://github.com/savirsingh/pytocpp',
+    keywords='py to cpp, pytocpp, python to c++',
+
+)
