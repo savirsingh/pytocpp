623e69b01d378dcae481ceb2191a2f65b726249e
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 19:10:24 -0400

    Create example4.cpp

---
+++ examples/example4.cpp
@@ -0,0 +1,11 @@
+// output for example4.py by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+string numstring =  "100000";int num = stoi(numstring);
+
+cout << num << endl;
+
+}
