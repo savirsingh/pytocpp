980ed13922823aa185e440b961d3cb1636090623
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 19:20:41 -0400

    Create example5.cpp

---
+++ examples/example5.cpp
@@ -0,0 +1,13 @@
+// output for example5.py by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+int num = 5000;
+string nums = to_string(num);
+
+cout << nums << endl;
+
+}
