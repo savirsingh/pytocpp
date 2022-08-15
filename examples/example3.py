e602347a11bbaa92bef05cb02946be2df458d022
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 19:06:16 -0400

    Create example3.py

---
+++ examples/example3.py
@@ -0,0 +1,10 @@
+# example by savir singh
+# shows how pytocpp converts lists to vectors
+# this example is intended to fail in python but work in c++
+
+justapythonlist = ["hello", "world", "bye", "planet"]
+a = int(input())
+b = input()
+
+print("The integer input is: " + a)
+print("The string input is: " + b)
