68031c48146b7361a34e949e85b396aa1978b63a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 19:03:50 -0400

    Create example2.py

---
+++ examples/example2.py
@@ -0,0 +1,11 @@
+# example by savir singh
+# shows input being taken and being converted to integers
+# also shows how integers can be outputted alongside strings in C++ (not in python, and therefore this script won't run in python)
+
+a = int(input())
+b = int(input())
+
+c = a*b
+
+for i in range(c):
+    print("Variable 'c' is equal to " + c)
