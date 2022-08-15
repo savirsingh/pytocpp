a35c91271776300b1fa8c72e2988ebc9a5c6bd4a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 19:04:21 -0400

    Update example2.cpp

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -1,1 +1,14 @@
+// output for example2.py by savir singh
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int a ;
+cin >> a ;int b ;
+cin >> b ;
+int c =  a*b;
+for (int i=0; i<c; i++) {    cout << "Variable 'c' is equal to "  <<  c << endl;
+
+}
+}
