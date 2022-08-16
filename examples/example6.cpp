d28c6c1daa8992272416b232be528ffec753151f
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 20:08:22 -0400

    Create example6.cpp

---
+++ examples/example6.cpp
@@ -0,0 +1,18 @@
+// output for example6.py by savir singh
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int x ;
+cin >> x ;int y ;
+cin >> y ;
+int xplusy =  x+y;int xtimesy =  x*y;int xminusy =  x-y;
+cout << "x plus y = "  <<  xplusy << endl;
+cout << "x times y = "  <<  xtimesy << endl;
+cout << "x minus y = "  <<  xminusy << endl;
+
+
+
+}
