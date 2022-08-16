776d084d120d592458432067e7912fc6fc2893f0
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-15 20:08:44 -0400

    Create example6.py

---
+++ examples/example6.py
@@ -0,0 +1,13 @@
+# example by savir singh
+# just a cool example
+
+x = int(input())
+y = int(input())
+
+xplusy = x+y
+xtimesy = x*y
+xminusy = x-y
+
+print("x plus y = " + xplusy)
+print("x times y = " + xtimesy)
+print("x minus y = " + xminusy)
