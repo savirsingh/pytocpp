0d51537c2ebef922345dd775a93b1302c330ea0b
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-11 18:20:52 -0400

    Update example1.cpp

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -5,11 +5,20 @@
 using namespace std;
 
 int main() {
-int a ;
-cin >> a ;int b ;
-cin >> b ;
-int c =  a*b;
-for (int i=0; i<c; i++) {    cout << "Variable 'c' is equal to "  <<  c << endl;
 
-}
+int a = 5;
+int b = 10;
+int c =  a+b;
+int d = 15;
+string string1 =  "hello";
+
+
+cout << c*d << endl;
+
+cout << string1 << endl;
+
+
+
+
+
 }
